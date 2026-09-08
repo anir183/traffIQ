905597574574537e53906d740954c2763158f75d
anir183 <188557785+anir183@users.noreply.github.com>
2026-09-08 22:29:54 +0530

    feat(frontend): logo goes hard

--- frontend/src/header/name.tsx
+++ frontend/src/header/name.tsx
@@ -1,10 +1,10 @@
-import Logo from '../assets/Union.svg'
+import Logo from '../assets/logo.svg'
 
 function App() {
   return (
     <div className="flex items-center gap-2">
-      <span className="flex items-center">
-        <img src={Logo} alt="TraffIQ logo" className="h-8 w-auto" />
+      <span className="flex items-center p-1">
+        <img src={Logo} alt="TraffIQ logo" className="h-6 w-auto" />
       </span>
       <span className="text-xl font-semibold tracking-tight text-slate-900 dark:text-slate-100">
         traff<span className="text-blue-600">IQ</span>
