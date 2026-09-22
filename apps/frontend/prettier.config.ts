9e3ae1dc6e1d842210b407f55c92810ed3356f47
anir183 <188557785+anir183@users.noreply.github.com>
2026-09-22 23:31:51 +0530

    feat(frontend): react ts project bootstrap
    mise frontend
    
    name

---
+++ apps/frontend/prettier.config.ts
@@ -0,0 +1,13 @@
+// prettier.config.ts
+
+import type { Config } from "prettier";
+
+const config: Config = {
+    semi: true,
+    singleQuote: false,
+    trailingComma: "all",
+    tabWidth: 4,
+    printWidth: 80,
+};
+
+export default config;
---
+++ apps/frontend/src/App.tsx
@@ -0,0 +1,19 @@
+function App() {
+    return (
+        <section className="flex min-h-screen items-center justify-center px-6">
+            <div className="max-w-2xl text-center">
+                <h1 className="text-4xl font-bold tracking-tight">
+                    Hello! This is TraffIQ.
+                </h1>
+
+                <p className="mt-4 text-lg text-gray-600">
+                    An AI-powered traffic intelligence platform that connects
+                    isolated CCTV and ANPR cameras into a unified city-wide
+                    vehicle tracking and analytics system.
+                </p>
+            </div>
+        </section>
+    );
+}
+
+export default App;
